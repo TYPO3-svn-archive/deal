98819082c9599fbc81f3f00c6732cde80485a57b
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-02-08 12:44:29 +0000

    0.0.1 - Initial Release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/deal/trunk@83697 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,38 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Deal - Quick Shop interface for amazon and ebay.',
+	'description' => 'TYPO3 Quick Shop interface for the marketplaces amazon and ebay. Manage all items with TYPO3 and publish it at amazon and ebay. Use the interface ready-to-use. See: https://typo3-deal.de/typo3conf/ext/deal/doc/manual.pdf',
+	'category' => 'be',
+	'shy' => 0,
+	'version' => '0.0.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Dirk Wildt (Die Netzmacher)',
+	'author_email' => 'http://wildt.at.die-netzmacher.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5.0-6.1.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+);
+
+?>
